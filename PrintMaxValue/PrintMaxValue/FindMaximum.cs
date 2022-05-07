7013c8adb7699d8681a0ab547336e42663f958db
vrb9999 <vbiradar9999@gmail.com>
2022-05-07 23:56:31 +0530

    UC-5 Day-13 Print max value

---
+++ PrintMaxValue/PrintMaxValue/FindMaximum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintMaxValue
+{
+    public class FindMaximum<T> where T : IComparable
+    {
+        public T[] array;
+        public FindMaximum(T[] array)
+        {
+            this.array = array;
+        }
+        public T[] Sort()
+        {
+            Array.Sort(array);
+            return array;
+        }
+        public T FindMaxValue()
+        {
+            T[] sorted = this.Sort();
+            return sorted[sorted.Length - 1];
+        }
+        public void PrintMaxValue()
+        {
+            Console.WriteLine(this.FindMaxValue());
+        }
+    }
+}
---
+++ PrintMaxValue/PrintMaxValueTests/FindMaximumTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrintMaxValue;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintMaxValue.Tests
+{
+    [TestClass()]
+    public class FindMaximumTests
+    {
+        [TestMethod]
+        public void Passing_Integer_Array_Should_Return_Maximum_Value()
+        {
+            int[] intArray = { 10, 20, 30 };
+            int expected = 30;
+            FindMaximum<int> find = new FindMaximum<int>(intArray);
+            int actual = find.FindMaxValue();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Passing_float_Array_Should_Return_Maximum_Value()
+        {
+            float[] floatArray = { 10.5f, 20.5f, 30.5f };
+            float expected = 30.5f;
+            FindMaximum<float> find = new FindMaximum<float>(floatArray);
+            float actual = find.FindMaxValue();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Passing_string_Array_Should_Return_Maximum_Value()
+        {
+            string[] strArray = { "10", "20", "30" };
+            string expected = "30";
+            FindMaximum<string> find = new FindMaximum<string>(strArray);
+            string actual = find.FindMaxValue();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
